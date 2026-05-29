180d90b927fa3dba860950779d5bdda6436eaf3f
js504040 <js504040@lgupluspartners.co.kr>
2026-05-29 13:28:54 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/gildongmu/gildongmu_backend/GildongmuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gildongmu.gildongmu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GildongmuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GildongmuBackendApplication.class, args);
+	}
+
+}
