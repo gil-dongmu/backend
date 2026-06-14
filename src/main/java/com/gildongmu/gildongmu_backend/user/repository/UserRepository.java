1204e9e7af40c313a1157f83dfbdfcf29f65ac21
seongeheonjeong <js504040@naver.com>
2026-06-14 19:41:10 +0900

    feat: 에러 코드 추가 및 닉네임 유니크 검증 쿼리
    - ErrorCode: USER_NOT_FOUND 추가 (404)
    - ErrorCode: DUPLICATE_NICKNAME 추가 (409)
    - UserRepository: existsByNickname 메서드 추가

--- src/main/java/com/gildongmu/gildongmu_backend/user/repository/UserRepository.java
+++ src/main/java/com/gildongmu/gildongmu_backend/user/repository/UserRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByProviderAndProviderId(Provider provider, String providerId);
+
+    boolean existsByNickname(String nickname);
 }
